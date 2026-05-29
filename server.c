4102c4824a6c411cd7264060c28e32471fef16a5
Miles Bi <miles.bi.jiayu@gmail.com>
2026-05-28 19:02:24 -0500

    handle connections function

--- server.c
+++ server.c
@@ -2,18 +2,14 @@
 
 #include <netinet/in.h>
 #include <sys/socket.h>
+
 //  #include <netdb.h>
-
-//  #include <sys/signal.h>
-
+//  #include <signal.h>
 //  #include <unistd.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-
-//  #define MAXSIZE 1024 //max-bytes for read-buffer
-
 
 int initialize_server(int port_num, int backlog) {
 
@@ -51,82 +47,72 @@ int initialize_server(int port_num, int backlog) {
     return sock_ds;
 }
 
+void handle_connections(int sock_ds, int max_size) {
+
+    // create client IPv4 address
+    struct sockaddr_in rem_addr;
+    socklen_t length = sizeof(rem_addr);
+
+    // create text buffer
+    char buff[max_size];
+    
+    // main handling loop
+    while (1) {
+
+        // check for client connections
+        int acc_ds = accept(sock_ds, (struct sockaddr *)&rem_addr, &length);
+        if (acc_ds == -1) {
+            perror("Accept error");
+            continue;
+        }
+        
+        if (fork() == 0) {
+            close(sock_ds);
+
+            memset(buff, 0, max_size);
+            int num_bytes = read(acc_ds, buff, max_size);
+            while (num_bytes > 0) {
+                printf("Message from remote host: %s\n", buff);
+                if (strcmp(buff, "quit") == 0) {
+                    break;
+                }
+                memset(buff, 0, max_size);
+            }
+
+            write(acc_ds, "Reading done", 13);
+            close(acc_ds);
+            exit(0);
+        }
+        else {
+            close(acc_ds);
+        }
+    }
+}
+
 int main() {
 
-    int port_num = 3333;
-    int backlog = 5;
+    // PARAMETERS
+    int port_num = 3333;     // port number
+    int backlog = 5;         // maximum queue size
+    int max_buf_size = 1024; // maximum size of the text buffer
 
+    // prevent zombie processes
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_NOCLDWAIT;  
+    sigaction(SIGCHLD, &sa, NULL);
+
+    // initialize the server
     int sock_ds = initialize_server(port_num, backlog);
     if (sock_ds == -1) {
         perror("Server initialization failed");
         exit(1);
     }
+    printf("Server successfully initialized, listening on port %d\n", port_num);
 
-    printf("Server initialized, listening on port %d!\n", port_num);
-
-    getchar();
-
-    // struct sigaction sa;
-    // sa.sa_handler = SIG_IGN;
-    // sigemptyset(&sa.sa_mask);
-    // sa.sa_flags = SA_NOCLDWAIT;  
-    // sigaction(SIGCHLD, &sa, NULL);
-
-
+    // handle connections from clients
+    handle_connections(sock_ds, max_buf_size);
 
     return 0;
-
-    // int sock_ds, ret, length; // server socket descriptor
-    // int acc_ds; // client socket descriptor
-
-    // struct sockaddr_in addr; // server address
-    // struct sockaddr rem_addr; // generic client address
-
-    // char buff[MAXSIZE];
-
-    // sock_ds = socket(AF_INET, SOCK_STREAM, 0); // => TCP
-
-    // bzero((char *)&addr, sizeof(addr)); //reset struct
-    // addr.sin_family = AF_INET;
-    // addr.sin_port = htons(25000);
-    // addr.sin_addr.s_addr = INADDR_ANY;
-
-    // ret = bind(sock_ds, &addr, sizeof(addr));
-    // if(ret == -1){
-    //     perror("Binding error");
-    //     exit(1);
-    // }
-
-    // ret = listen(sock_ds, BACKLOG); // backlog queue
-    //     if(ret == -1){
-    //     perror("Listen error");
-    //     exit(1);
-    // }
-
-    // length = sizeof(rem_addr);
-    // signal(SIGCHLD, SIG_IGN); //zombie children management
-
-    // /*Busy-waiting (server) and concurrency */
-    // while(1){
-
-    //     /*Repeat until success*/
-    //     while(acc_ds = accept(sock_ds, &rem_addr, &length) == -1){
-
-    //         if(fork() == 0){ //child-process
-
-    //             close(sock_ds); //unused from child
-    //             do{
-    //                 read(acc_ds, buff, MAXSIZE);
-    //                 printf("Message from remote host:&s\n", buff);
-
-    //             }while(strcmp(buff, "quit") == 0);
-    //             /*Transimission completed: server response  */
-    //             write(acc_ds, "Reading Done", 10);
-    //             close(acc_ds); //socket closed
-    //             exit(0); //exiting from child
-    //         }
-    //         else{
-    //             close(acc_ds); //unused from parent
-    //         }
-    //     }
 }
